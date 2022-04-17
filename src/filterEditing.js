ba69f77268da7c6e5fc630d025900d0618fd9639
cat2151 <fct_2004@mail.goo.ne.jp>
2022-04-17 16:45:58 +0900

    編集中ファイル絞り込みの半透明とウィンドウサイズを、コマンドパレットに寄せた

--- src/filterEditing.js
+++ src/filterEditing.js
@@ -33,7 +33,7 @@ function deleteFile(filename, fso) {
 
 function filterEditing(macroDir, nowEditName, resultName, migemoDict, encode) {
   var exeName = macroDir + "miniIncrementalSearchFilter\\miniIncrementalSearchFilter.exe";
-  var cmd = exeName + " " + nowEditName + " " + resultName + " --encode " + encode + " --andsearch --migemo " + migemoDict;
+  var cmd = exeName + " " + nowEditName + " " + resultName + " --encode " + encode + " --andsearch --migemo " + migemoDict + " --alpha 0.96 --width 150 --height 40"
   execCmd(cmd);
 }
 
